10f3712caa74ebdc4ba72179c7e52a63fb140349
Meet Solanki <99175906+meets4@users.noreply.github.com>
2024-05-19 14:10:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const typedText = new Typed(".text", {
+        strings: ["Frontend Developer", "Web Developer", "React Developer"],
+        typeSpeed: 70,
+        backSpeed: 30,
+        backDelay: 1000,
+        loop: true
+    });
+});
