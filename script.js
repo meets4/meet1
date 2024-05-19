e87a0b272b80f4009509a72846bbeaa1e08e1ae6
Meet Solanki <99175906+meets4@users.noreply.github.com>
2024-05-19 17:39:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ document.addEventListener('DOMContentLoaded', function() {
         backDelay: 1000,
         loop: true
     });
+
+    const menuToggle = document.getElementById('menuToggle');
+    const menubar = document.getElementById('menubar');
+    const container = document.querySelector('.main-container');
+
+    menuToggle.addEventListener('click', function(event) {
+        menubar.style.transform = 'translateX(0px)';
+        menubar.style.opacity = '1';
+        menubar.style.transition = '0.4s';
+        container.style.transition = '0.4s';
+        container.style.filter = 'blur(10px)'
+        event.stopPropagation(); // Prevent click from propagating to document
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!menubar.contains(event.target) && !menuToggle.contains(event.target)) {
+            menubar.style.transform = 'translateX(-100%)'; // or however you want to hide the menubar
+            container.style.filter = 'blur(0px)'
+        }
+    });
 });
